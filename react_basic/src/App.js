de7cd5255180515c5e3346856694022b5bcc4dbb
Brent Coombs <112416322+brent-coombs-bytecode@users.noreply.github.com>
2024-10-28 16:03:27 -0600

    adding pic and button

--- react_basic/src/App.js
+++ react_basic/src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+
+<img className="avatar" />
+
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <MyButton />
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,10 +40,27 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>    
+
+    <h1>{user.name}</h1>
+    <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />            
       </header>
+
+
+
+      
     </div>
   );
+
+  
 }
 
 export default App;
